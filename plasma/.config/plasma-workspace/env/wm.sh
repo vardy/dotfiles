92b0b13c59e60228332e660c18141a8fcd5832cd
vardy <dwarvenplague@gmail.com>
2018-11-16 00:13:09 +0800

    Removed support for herbstluftwm

--- plasma/.config/plasma-workspace/env/wm.sh
+++ plasma/.config/plasma-workspace/env/wm.sh
@@ -1,7 +1,6 @@
 # Disable KWin and use i3gaps as WM
 export KDEWM=/usr/bin/i3
 #export KDEWM=$HOME/Documents/Repositories/uwurawrxdwm/uwurawrxdwm
-#export KDEWM=/usr/bin/herbstluftwm
 
 # Compositor (Animations, Shadows, Transparency)
 # xcompmgr -C
